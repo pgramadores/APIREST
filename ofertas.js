785d2e514fded4e524e8f07d09db617955e2d6dd
Juan Mora <raikcerk@gmail.com>
2016-09-24 15:28:17 -0300

    Se crea esquema de la base de datos

---
+++ ofertas.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
+
+var ofertasSchema = new Schema({
+    cargo:  { type: String },
+    publicador: {type: String },
+    correopublicador: {type: String },
+    categoria: {type: String, enum:['Desarrollo','Redes','Arquitectura','Administrativo','Diseño']},
+    sueldo:   { type: Number },
+    tipocontrato: {type: String, enum:['Indefinido', 'Honorarios', 'Proyecto', 'Otro']},
+    descripciongeneral: {type; String },
+    beneficiosventajas : {type: String },
+    requisitos: {type: String },
+    pais:{ type: String },
+    imagen: { type: String },
+    fechacreacion: {type: Date },
+    fechatermino: {type: Date },
+    estado:{ type: Boolean }
+});
+
+module.exports = mongoose.model('Ofertas', ofertasSchema);
