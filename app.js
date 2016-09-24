963b00ec0a57fe4fe743c035ac82be5f0a532a75
Juan Mora <raikcerk@gmail.com>
2016-09-24 17:30:44 -0300

    se agregan nuevos metodos y se cambian algunos por error al lanzar la aplicacion, y al inmtentar realizar metodos GET o POST

--- app.js
+++ app.js
@@ -4,25 +4,46 @@ var express = require("express"),
     methodOverride = require("method-override");
     mongoose = require('mongoose');
 
+//Conexion con la base de datos
+mongoose.connect('mongodb://localhost/Ofertas', function(err, res) {
+    if(err) {
+        console.log('ERROR: connecting to Database. ' + err);
+    }else {
+        console.log('Conectado a mongodb');
+    }
+});
+
+// Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+//Modelo y controlador
+// Import Models and controllers
+var ofertas_     = require('./ofertas')(app, mongoose);
+var OfertasCtrl = require('./routes');
+
 var router = express.Router();
-
 router.get('/', function(req, res) {
-   res.send("Hello World! weiwno");
+  res.send("Hello world!");
 });
-
 app.use(router);
 
-mongoose.connect('mongodb://localhost/Ofertas', function(err, res) {
-  if(err) {
-    console.log('ERROR: connecting to Database. ' + err);
-}else {
-    console.log('Conectado a mongodb');
-}
-  app.listen(3000, function() {
+// API routes
+var ofertas_ = express.Router();
+app.use(ofertas_);
+
+ofertas_.route('/ofertas')
+  .get(OfertasCtrl.findAllOfertas)
+  .post(OfertasCtrl.addOferta);
+
+ofertas_.route('/ofertas/:id')
+  .get(OfertasCtrl.findById)
+  .put(OfertasCtrl.updateOferta)
+  .delete(OfertasCtrl.deleteOferta);
+
+app.use('/api', ofertas_);
+
+app.listen(3000, function() {
     console.log("Node server running on http://localhost:3000");
-  });
 });
